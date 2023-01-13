dc2073f4c685012717af3aa566f207740dfcd7e2
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-01-13 12:30:04 +0000

    Change structure to be OOP; create classes

--- game.js
+++ game.js
@@ -1,125 +1,185 @@
 const prompt = require("prompt-sync")();
 const color = require("ansi-colors")
 
-
-let possible_choices = ['r', 'p', 's']
-let extra_choices = ['q', 're']
-let possible_user_inputs = possible_choices + extra_choices
-let full_name = {
-    'r':'Rock',
-    'p':'Paper',
-    's':'Scissors'
-}
-
-
-
-const get_user_choice = () => {
-    let user_choice
-    while (!user_choice) {
-        user_choice = prompt("Please select Rock/Paper/Scissors (type R/P/S) (Q = Quit, RE = Get results): ");
-        user_choice.toLowerCase();
-        if (!possible_user_inputs.includes(user_choice)) {
-            console.log("Please make sure you type either R/P/S");
-            user_choice = "";
-        }
-    } 
-    return user_choice
-}
-
-const get_computer_choice = () => {
-    let random_int = Math.floor(Math.random() * possible_choices.length)
-    let computer_choice = possible_choices[random_int]
-    return computer_choice
-}
-
-const determine_result = (user_choice, computer_choice) => {
-    if (user_choice === computer_choice) {
-        return "draw"
+class Game {
+    constructor() {
+        this.possibleChoices = ['r', 'p', 's']
     }
 
-    if (user_choice === "r") {
-        if (computer_choice === "p") {
-            return "lose"
+    play() {
+        let user = new User(this.possibleChoices)
+        let computer = new Computer(this.possibleChoices)
+        let round = new Round(user, computer, 1)
+
+        while (true) {
+            round.playRound()
+        }
+    }
+}
+
+class Round {
+
+    constructor(user, computer, number) {
+        this.user = user
+        this.computer = computer
+        this.number = number
+        this.previousResult = {}
+    }
+
+    playRound() {
+        this.user.getChoice()
+        if (this.user.choice === 'q') {
+            return
+        }
+        this.user.choice = new Choice(this.user.choice)
+        console.log(`\nRound ${this.number}:`)
+        this.computer.getChoice()
+        this.determineResult()
+        this.user.result = this.createRoundResultObject()
+        this.user.updateResults(this)
+        this.printRoundResults()
+        this.number ++
+    }
+
+    createRoundResultObject() {
+        return {
+            roundNumber: this.number, 
+            userChoice: this.user.choice, 
+            computerChoice: this.computer.choice, 
+            userResult: this.userResult
+        }
+    }
+
+    determineResult() {
+        if (this.user.choice.decision === this.computer.choice.decision) {
+            this.userResult = "draw"
+            return 
+        }
+    
+        if (this.user.choice.decision === "r") {
+            if (this.computer.choice.decision === "p") {
+                this.userResult = "lose"
+            } else {
+                this.userResult = "win"
+            } return 
+        }
+    
+        if (this.user.choice.decision === "p") {
+            if (this.computer.choice.decision === "s") {
+                this.userResult = "lose"
+            } else {
+                this.userResult = "win"
+            } return 
+        }
+    
+        if (this.user.choice.decision === "s") {
+            if (this.computer.choice.decision === "r") {
+                this.userResult = "lose"
+            } else {
+                this.userResult = "win"
+            } return 
+        }
+    }
+
+    printRoundResults() {
+        let message = `User choice: ${this.user.choice.fullName()}`
+        message += `\nComputer Choice: ${this.computer.choice.fullName()}`
+        message += `\nRound result: ${this.getResultText(this.userResult)}`
+        console.log(message)
+    }
+
+    getResultText(roundResult) {
+        if (roundResult === 'win') {
+            return color.green(roundResult.toUpperCase())
+        } else if (roundResult === 'lose') {
+            return color.red(roundResult.toUpperCase())
         } else {
-            return "win"
-        }
-    }
-
-    if (user_choice === "p") {
-        if (computer_choice === "s") {
-            return "lose"
-        } else {
-            return "win"
-        }
-    }
-
-    if (user_choice === "s") {
-        if (computer_choice === "r") {
-            return "lose"
-        } else {
-            return "win"
+            return color.yellow(roundResult.toUpperCase())
         }
     }
 }
 
-const get_text_color = (round_result) => {
-    if (round_result === 'win') {
-        return color.green(round_result.toUpperCase())
-    } else if (round_result === 'lose') {
-        return color.red(round_result.toUpperCase())
-    } else {
-        return color.yellow(round_result.toUpperCase())
+class Choice {
+
+    constructor(decision) {
+        this.decision = decision
+    }
+
+    fullName() {
+        let fullNames = {
+            'r':'Rock',
+            'p':'Paper',
+            's':'Scissors'
+        }
+        return fullNames[this.decision]
     }
 }
 
-const print_results = (round_result) => {
-    let message = `User choice: ${full_name[round_result.user_choice]}`
-    message += `\nComputer Choice: ${full_name[round_result.computer_choice]}`
-    message += `\nRound result: ${get_text_color(round_result.user_result)}`
-    console.log(message)
-}
-
-const update_user_results = (round_result, user_results) => {
-    user_results.roundsPlayed ++ 
-    if (round_result.user_result === 'win') {
-        user_results.wins ++ 
-    } else if (round_result.user_result === 'lose') {
-        user_results.losses ++ 
-    } else {
-        user_results.draws ++ 
+class User {
+    
+    constructor(possibleChoices) {
+        this.choice = null
+        this.result = null
+        this.results = {
+            "roundsPlayed":0,
+            "wins":0,
+            "losses":0,
+            "draws":0,
+            "lastRoundResults":{}
+        }
+        this.possibleChoices = possibleChoices
+        this.extraChoices = ['q', 're']
+        this.userInputs = this.possibleChoices.concat(this.extraChoices)
     }
-    user_results.lastRoundResults = round_result
-}
 
-const play_game = () => {
-    let user_results = {
-        "roundsPlayed":0,
-        "wins":0,
-        "losses":0,
-        "draws":0,
-        "lastRoundResults":{}
-    }
-    let round_number = 0
-    while (true) {
-        console.log("New Round...")
-        let user_choice = get_user_choice()
-        if (user_choice === "q") { break }
-
-        if (user_choice === "re") {
-            console.log(`User Results:\n${user_results}`)
-            continue
+    getChoice() {
+        let userChoice
+        while (!this.possibleChoices.includes(userChoice)) {
+            userChoice = prompt("Please select Rock/Paper/Scissors (type R/P/S) (Q = Quit, RE = Get results): ");
+            userChoice.toLowerCase();
+            if (!this.userInputs.includes(userChoice)) {
+                console.log("Please make sure you type either R/P/S");
+                userChoice = "";
+            }
+            if (userChoice === 're') {
+                this.printResults()
+            }
         } 
+        this.choice = userChoice
+    }
 
-        round_number ++ 
-        console.log(`\nRound ${round_number}:`)
-        let computer_choice = get_computer_choice()
-        let user_result = determine_result(user_choice, computer_choice)
-        let round_result = {user_result, round_number, user_choice, computer_choice}
-        
-        print_results(round_result)
-        update_user_results(round_result, user_results)        
+    printResults() {
+        console.log(`User Results:\n${JSON.stringify(this.results, null, 4)}`)
+    }
+
+    updateResults(round) {
+        this.results.roundsPlayed ++ 
+        if (round.userResult === 'win') {
+            this.results.wins ++ 
+        } else if (round.userResult === 'lose') {
+            this.results.losses ++ 
+        } else {
+            this.results.draws ++ 
+        }
+        this.results.lastRoundResults = round.user.result
+    }
+}
+
+class Computer {
+    
+    constructor(possibleChoices) {
+        this.possibleChoices = possibleChoices
+        this.choice = null
+    }
+
+    getChoice(possibleChoices) {
+        let random_int = Math.floor(Math.random() * this.possibleChoices.length)
+        let computerChoice = this.possibleChoices[random_int]
+        this.choice = new Choice(computerChoice)
     }
 }
 
 
-play_game()
+game = new Game()
+
+game.play()
